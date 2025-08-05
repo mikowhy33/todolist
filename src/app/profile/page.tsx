ddf9cdd865d70c3e14012f2ddb26c8905cff92a9
zad_data_uro <mikolaj.babicz@microsoft.wsei.edu.pl>
2025-08-05 07:22:10 +0200

    jakarta1_todo

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function Component() {
+	return (
+		<div className="first_class h-screen w-full flex justify-center items-center bg-blue-600 ">
+			<h1>Show smth jakarta</h1>
+		</div>
+	);
+}
